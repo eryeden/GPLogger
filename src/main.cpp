95250b7c4942229408e95ee6cd6e85f37a7f64dd
Kazuki Kikuchi <ery.eden@gmail.com>
2016-01-30 01:16:19 +0900

    add cmake files and configurations

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+
+
+#include <serial/serial.h>
+
+
+using namespace serial;
+
+using std::string;
+
+
+int main(){
+  Serial port1("/dev/ttyS0");
+
+  port1.close();
+
+  return 1;
+}
+
+
+
+
+
+
+
+
