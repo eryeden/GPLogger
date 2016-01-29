d2110f16f28cdcc659cce459514cb17812e76f0f
Kazuki Kikuchi <ery.eden@gmail.com>
2016-01-30 02:03:42 +0900

    mod

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,14 @@
 
 #include <serial/serial.h>
 
-
 using namespace serial;
 
 using std::string;
 
 
 int main(){
-  Serial port1("/dev/ttyS0");
 
+  Serial port1("/dev/ttyS0");	
   port1.close();
 
   return 1;
